02fddd27b77ab391e0293552a836b8be32cfc27b
Tim Franssen <tim.franssen@nedap.com>
2020-02-17 15:32:25 +0100

    Show full animation before returning to remote observations

--- client/components/irma-login.js
+++ client/components/irma-login.js
@@ -32,7 +32,7 @@ export default {
     try {
       const result = await irma.start();
       window.irmaLogin = true;
-      window.history.back();
+      window.setTimeout(() => window.history.back(), 1200);
     } catch (e) {
       console.error(`Trouble running IRMA flow: `, e);
     }

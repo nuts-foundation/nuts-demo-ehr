8b9d117b6c2a2f88235f702ec1ef32cad42a71d8
Tim Franssen <tim.franssen@nedap.com>
2020-02-20 21:36:21 +0100

    Get rid of the old event helpers and instead support "server-internal events"

--- util/events.js
+++ util/events.js
@@ -1,46 +1,8 @@
-module.exports = subscriptionCallback => {
+// Module that takes care of server-internal events
 
-  const events = require('express').Router();
-  let clients  = [];
-  const lastMessages = {};
+const EventEmitter = require('events');
+class AccessLogEmitter extends EventEmitter {}
 
-  events.get('/:topic', (req, res) => {
-    // Headers to keep connection open
-    res.writeHead(200, {
-      'Content-Type':  'text/event-stream',
-      'Connection':    'keep-alive',
-      'Cache-Control': 'no-cache'
-    });
-
-    // Add client and requested topic to the client list
-    const client = {
-      topic: req.params.topic,
-      res
-    };
-    clients.push(client);
-
-    // Clean up client when connection closes
-    req.on('close', () => clients = clients.filter(c => c !== client));
-
-    // Send client last known message for this topic
-    const lastMessage = lastMessages[req.params.topic];
-    if ( lastMessage )
-      res.write(`data: ${JSON.stringify(lastMessage)}\n\n`);
-
-    // Notify outside world of new client
-    subscriptionCallback(client);
-  });
-
-  events.publish = (options) => {
-    lastMessages[options.topic] = options.message;
-    clients.filter(c => c.topic == options.topic)
-           .forEach(c => c.res.write(`data: ${JSON.stringify(options.message)}\n\n`));
-  };
-
-  events.topics = () => {
-    return [...new Set(clients.map(c => c.topic))]; // Unique topics
-  }
-
-  return events;
-
-};
+module.exports = {
+  accessLog: new AccessLogEmitter()
+}

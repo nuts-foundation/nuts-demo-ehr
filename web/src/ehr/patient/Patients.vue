a42a4910e19f046500b2e7e1bced55c342c849e5
stevenvegt <steven.vandervegt@nedap.com>
2021-07-02 10:40:25 +0200

    Nicer UI markup
    Fix header and add patient button

--- web/src/ehr/patient/Patients.vue
+++ web/src/ehr/patient/Patients.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <h1 class="text-xl">Patients</h1>
+    <h1 class="text-2xl">Patients</h1>
 
     <p v-if="!!error" class="m-4">Error: {{ error }}</p>
 
-    <router-link :to="{name:'ehr.patients.new'}">Add new Patient</router-link>
+    <button
+        @click="$router.push({name: 'ehr.patients.new'})"
+        class="float-right inline-flex items-center"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+      </svg>
+      Add new Patient</button>
     <table class="min-w-full divide-y divide-gray-200">
       <thead class="bg-gray-50">
       <tr>

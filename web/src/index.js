f08f43c5c22ffddd7d10dda45427a77080cb141c
stevenvegt <steven.vandervegt@nedap.com>
2021-07-08 10:44:30 +0200

    Add an autocomplete

--- web/src/index.js
+++ web/src/index.js
@@ -12,103 +12,105 @@ import Api from './plugins/api'
 import Patients from './ehr/patient/Patients.vue'
 import Patient from './ehr/patient/Patient.vue'
 import NewPatient from './ehr/patient/NewPatient.vue'
-import EditPatient from "./ehr/patient/EditPatient.vue";
-import NewDossier from "./ehr/patient/dossier/New.vue";
-import TransferDossier from "./ehr/patient/dossier/Transfer.vue";
-import Settings from "./ehr/Settings.vue";
+import EditPatient from "./ehr/patient/EditPatient.vue"
+import NewDossier from "./ehr/patient/dossier/New.vue"
+import TransferDossier from "./ehr/patient/dossier/Transfer.vue"
+import Settings from "./ehr/Settings.vue"
+import Components from "./Components.vue"
 
 const routes = [
-    {path: '/', component: Login},
-    {
-        name: 'login',
-        path: '/login',
-        component: Login
+  {path: '/', component: Login},
+  {
+    name: 'login',
+    path: '/login',
+    component: Login
+  },
+  {
+    name: 'logout',
+    path: '/logout',
+    component: Logout
+  },
+  {
+    name: 'auth.passwd',
+    path: '/auth/passwd/',
+    component: PasswordAuthentication,
+  },
+  {
+    name: 'auth.irma',
+    path: '/auth/irma/',
+    component: IRMAAuthentication,
+  },
+  {
+    path: '/ehr',
+    components: {
+      default: EHRApp,
     },
-    {
-        name: 'logout',
-        path: '/logout',
-        component: Logout
-    },
-    {
-        name: 'auth.passwd',
-        path: '/auth/passwd/',
-        component: PasswordAuthentication,
-    },
-    {
-        name: 'auth.irma',
-        path: '/auth/irma/',
-        component: IRMAAuthentication,
-    },
-    {
-        path: '/ehr',
-        components: {
-            default: EHRApp,
-        },
-        children: [
-            {
-                path: '',
-                name: 'ehr.home',
-                redirect: '/ehr/patients'
-            },
-            {
-                path: 'patients',
-                name: 'ehr.patients',
-                component: Patients
-            },
-            {
-                path: 'patients/new',
-                name: 'ehr.patients.new',
-                component: NewPatient
-            },
-            {
-                path: 'patient/:id',
-                name: 'ehr.patient',
-                component: Patient
-            },
-            {
-                path: 'patient/:id/edit',
-                name: 'ehr.patient.edit',
-                component: EditPatient
-            },
-            {
-                path: 'patient/:id/dossier/new',
-                name: 'ehr.patient.dossier.new',
-                component: NewDossier
-            },
-            {
-                path: 'patient/:id/transfer',
-                name: 'ehr.patient.transfer',
-                component: TransferDossier
-            },
-            {
-                path: 'settings',
-                name: 'ehr.settings',
-                component: Settings
-            }
-        ],
-        meta: {requiresAuth: true}
-    },
-    {path: '/:pathMatch*', name: 'NotFound', component: NotFound}
+    children: [
+      {
+        path: '',
+        name: 'ehr.home',
+        redirect: '/ehr/patients'
+      },
+      {
+        path: 'patients',
+        name: 'ehr.patients',
+        component: Patients
+      },
+      {
+        path: 'patients/new',
+        name: 'ehr.patients.new',
+        component: NewPatient
+      },
+      {
+        path: 'patient/:id',
+        name: 'ehr.patient',
+        component: Patient
+      },
+      {
+        path: 'patient/:id/edit',
+        name: 'ehr.patient.edit',
+        component: EditPatient
+      },
+      {
+        path: 'patient/:id/dossier/new',
+        name: 'ehr.patient.dossier.new',
+        component: NewDossier
+      },
+      {
+        path: 'patient/:id/transfer',
+        name: 'ehr.patient.transfer',
+        component: TransferDossier
+      },
+      {
+        path: 'settings',
+        name: 'ehr.settings',
+        component: Settings
+      }
+    ],
+    meta: {requiresAuth: true}
+  },
+  {path: '/components', component: Components},
+  {path: '/:pathMatch*', name: 'NotFound', component: NotFound}
 ]
 
 const router = createRouter({
-    // We are using the hash history for simplicity here.
-    history: createWebHashHistory(),
-    routes // short for `routes: routes`
+  // We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes // short for `routes: routes`
 })
 
 const app = createApp(App)
 
 router.beforeEach((to, from) => {
-    console.log("from: ", from.path, from.name, "to: ", to.path, to.name)
-    // Check before rendering the target route that we're authenticated, if it's required by the particular route.
-    if (to.meta.requiresAuth === true) {
-        if (localStorage.getItem("session")) {
-            return true
-        }
-        console.log("no cookie found, redirect to login")
-        return '/login'
+  console.log("from: ", from.path, from.name, "to: ", to.path, to.name)
+  // Check before rendering the target route that we're authenticated, if it's required by the particular route.
+  if (to.meta.requiresAuth === true) {
+    if (localStorage.getItem("session")) {
+      return true
     }
+    console.log("no cookie found, redirect to login")
+    return '/login'
+  }
 })
 
 app.use(router)

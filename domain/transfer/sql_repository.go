af55e7aac003368a2098910680b6a0703e6a1199
stevenvegt <steven.vandervegt@nedap.com>
2021-07-12 16:38:43 +0200

    Implement transfer.Repository.Create

--- domain/transfer/sql_repository.go
+++ domain/transfer/sql_repository.go
@@ -1,17 +1,31 @@
 package transfer
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/nuts-foundation/nuts-demo-ehr/domain"
 )
 
 type sqlTransfer struct {
 	ID          string       `db:"id"`
 	CustomerID  string       `db:"customer_id"`
-	Dob         sql.NullTime `db:"date"`
-	status      string       `db:"status"`
-	description string       `db:"description"`
+	Date        sql.NullTime `db:"date"`
+	Status      string       `db:"status"`
+	Description string       `db:"description"`
+}
+
+func (dbTransfer *sqlTransfer) UnmarshalFromDomainTransfer(customerID string, transfer domain.Transfer) error {
+	*dbTransfer = sqlTransfer{
+		ID:          string(transfer.Id),
+		Date:        sql.NullTime{Time: transfer.TransferDate.Time, Valid: !transfer.TransferDate.IsZero()},
+		CustomerID:  customerID,
+		Status:      string(transfer.Status),
+		Description: transfer.Description,
+	}
+	return nil
 }
 
 const schema = `
@@ -41,4 +55,45 @@ func NewSQLiteTransferRepository(factory Factory, db *sqlx.DB) *SQLiteTransferRe
 		factory: factory,
 		db:      db,
 	}
+}
+
+func (r SQLiteTransferRepository) FindByID(ctx context.Context, customerID, id string) (*domain.Transfer, error) {
+	panic("implement me")
+}
+
+func (r SQLiteTransferRepository) FindByPatientID(ctx context.Context, customerID, patientID string) []domain.Transfer {
+	panic("implement me")
+}
+
+func (r SQLiteTransferRepository) Create(ctx context.Context, customerID, dossierID, description string, date time.Time) (*domain.Transfer, error) {
+	transfer := r.factory.NewTransfer(description, date)
+	dbTransfer := sqlTransfer{}
+	if err := dbTransfer.UnmarshalFromDomainTransfer(customerID, *transfer); err != nil {
+		return nil, err
+	}
+	const query = `INSERT INTO transfer 
+		(id, customer_id, date, status, description)
+		values(:id, :customer_id, :date, :status, :description)
+`
+
+	if _, err := r.db.NamedExec(query, dbTransfer); err != nil {
+		return nil, err
+	}
+	return transfer, nil
+}
+
+func (r SQLiteTransferRepository) Update(ctx context.Context, customerID, description string, date time.Time, state domain.TransferStatus) error {
+	panic("implement me")
+}
+
+func (r SQLiteTransferRepository) Cancel(ctx context.Context, customerID, id string) {
+	panic("implement me")
+}
+
+func (r SQLiteTransferRepository) CreateNegotiation(ctx context.Context, transferID string, organizationDID string) (*domain.TransferNegotiation, error) {
+	panic("implement me")
+}
+
+func (r SQLiteTransferRepository) ListNegotiations(ctx context.Context, customerID, transferID string) ([]domain.TransferNegotiation, error) {
+	panic("implement me")
 }
--- domain/transfer/sql_repository_test.go
+++ domain/transfer/sql_repository_test.go
@@ -1,7 +1,9 @@
 package transfer
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
@@ -13,5 +15,32 @@ func TestNewSQLiteTransferRepository(t *testing.T) {
 		db := sqlx.MustConnect("sqlite3", ":memory:")
 		repo := NewSQLiteTransferRepository(Factory{}, db)
 		assert.NoError(t, repo.db.Ping())
+	})
+}
+
+func TestSQLiteTransferRepository_Create(t *testing.T) {
+	t.Run("create new transfer", func(t *testing.T) {
+		db := sqlx.MustConnect("sqlite3", ":memory:")
+		repo := NewSQLiteTransferRepository(Factory{}, db)
+		now := time.Now().UTC().Round(time.Minute)
+		newTransfer, err := repo.Create(context.Background(), "c1", "14", "foo", now)
+
+		if !assert.NoError(t, err) || !assert.NotNil(t, newTransfer) {
+			return
+		}
+		assert.NotEmpty(t, newTransfer.Id)
+		query := "SELECT * FROM `transfer` WHERE customer_id = ? ORDER BY id ASC"
+		rows, err := repo.db.Queryx(query, "c1", "14")
+		if !assert.NoError(t, err) {
+			return
+		}
+		dbTransfer := &sqlTransfer{}
+		rows.Next()
+		if !assert.NoError(t, rows.StructScan(dbTransfer)) {
+			return
+		}
+		assert.Equal(t, string(newTransfer.Id), dbTransfer.ID)
+		assert.Equal(t, newTransfer.TransferDate.Time, dbTransfer.Date.Time)
+		assert.Equal(t, newTransfer.Description, dbTransfer.Description)
 	})
 }

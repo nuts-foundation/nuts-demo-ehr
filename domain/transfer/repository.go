233adc5c901172eb64613b307857f424a548349c
stevenvegt <steven.vandervegt@nedap.com>
2021-07-12 16:38:43 +0200

    Implement transfer.Repository.Create

--- domain/transfer/repository.go
+++ domain/transfer/repository.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"time"
 
+	openapi_types "github.com/deepmap/oapi-codegen/pkg/types"
+	"github.com/google/uuid"
 	"github.com/nuts-foundation/nuts-demo-ehr/domain"
 )
 
 type Repository interface {
 	FindByID(ctx context.Context, customerID, id string) (*domain.Transfer, error)
 	FindByPatientID(ctx context.Context, customerID, patientID string) []domain.Transfer
-	Create(ctx context.Context, customerID, dossierID, description string, date time.Time) domain.Transfer
+	Create(ctx context.Context, customerID, dossierID, description string, date time.Time) (*domain.Transfer, error)
 	Update(ctx context.Context, customerID, description string, date time.Time, state domain.TransferStatus) error
 	Cancel(ctx context.Context, customerID, id string)
 	CreateNegotiation(ctx context.Context, transferID string, organizationDID string) (*domain.TransferNegotiation, error)
@@ -18,3 +20,12 @@ type Repository interface {
 }
 
 type Factory struct{}
+
+func (f Factory) NewTransfer(description string, date time.Time) *domain.Transfer {
+	return &domain.Transfer{
+		Id:           domain.ObjectID(uuid.NewString()),
+		Description:  description,
+		Status:       domain.TransferStatusCreated,
+		TransferDate: openapi_types.Date{Time: date},
+	}
+}

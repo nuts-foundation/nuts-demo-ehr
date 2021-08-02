514fe618edacc59b2756e3d6ce3b9b855c53e6dd
Rein Krul <info@reinkrul.nl>
2021-08-02 16:56:44 +0200

    PR feedback

--- web/src/ehr/patient/transfer/EditTransfer.vue
+++ web/src/ehr/patient/transfer/EditTransfer.vue
@@ -129,7 +129,7 @@ export default {
       this.requestedOrganization = null
     },
     searchOrganizations(query) {
-      this.$api.searchOrganizations({query: query})
+      this.$api.searchOrganizations({query: query, didServiceType: "eOverdracht-receiver"})
           .then((organizations) => {
             // Only show organizations that we aren't already negotiating with
             this.organizations = organizations.filter(i => this.negotiations.filter(n => i.did === n.organizationDID).length === 0)

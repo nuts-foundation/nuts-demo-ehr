6e233a4af4fc8860a1250ef75f2b04016a8863a0
stevenvegt <steven.vandervegt@nedap.com>
2021-09-08 12:58:29 +0200

    Handle aborts and allow to go back

--- web/src/components/auth/SessionElevation.vue
+++ web/src/components/auth/SessionElevation.vue
@@ -1,26 +1,35 @@
 <template>
 
-  <div v-if="!means">
-    Choose a means to elevate your session:
-    <button class="w-full btn btn-submit grid justify-items-center" @click="elevateWithIRMA">
-      <div>IRMA</div>
-      <img class="block my-3" v-bind:src="irmaLogo">
-    </button>
-    <button class="w-full btn btn-submit grid justify-items-center" @click="elevateWithBONO">
-      <div>BONO</div>
-    </button>
-    <button class="w-full btn btn-submit grid justify-items-center" @click="elevateWithDummy">
-      Dummy (for easy testing)
-    </button>
+  <div class="flex justify-center">
+
+    <div v-if="!means" class="mt-12 border rounded-md max-w-7xl p-8 flex flex-col">
+      <p>The page you want to access requires a higher lever of authentication.</p>
+
+      <p>Choose a means to elevate your session:</p>
+      <button class="w-full btn btn-submit grid justify-items-center" @click="elevateWithIRMA">
+        <div>IRMA</div>
+        <img class="block my-3" v-bind:src="irmaLogo">
+      </button>
+      <button class="w-full btn btn-submit grid justify-items-center" @click="elevateWithBONO">
+        <div>BONO</div>
+      </button>
+      <button class="w-full btn btn-submit grid justify-items-center" @click="elevateWithDummy">
+        Dummy (for easy testing)
+      </button>
+    </div>
+    <button v-else @click="means = ''">back</button>
+
+
+    <iframe v-if="means === 'bono'" width="560" height="315" src="https://www.youtube-nocookie.com/embed/19KstSgU-c0"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+
+    <irma-login v-if="means === 'irma'"
+                @success="onElevationSuccess"
+                @aborted="means = ''"
+    ></irma-login>
   </div>
-
-
-  <iframe v-if="means === 'bono'" width="560" height="315" src="https://www.youtube-nocookie.com/embed/19KstSgU-c0"
-          title="YouTube video player" frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen></iframe>
-
-  <irma-login v-if="means === 'irma'" @success="onElevationSuccess"></irma-login>
 </template>
 
 <script>

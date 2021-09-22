263a589845c4dcd32781b23a0c9dba8ab9776a57
Dillen Meijboom <info@excol.nl>
2021-09-22 12:09:06 +0200

    Fixed inbox

--- web/src/ehr/inbox/Inbox.vue
+++ web/src/ehr/inbox/Inbox.vue
@@ -1,51 +1,51 @@
 <template>
-  <div>
-    <h1 class="page-title">Inbox</h1>
+  <div class="px-12 py-8">
+    <h1 class="mb-6">Inbox</h1>
 
-    <table class="min-w-full divide-y divide-gray-200 mt-2">
-      <thead class="bg-gray-50">
-      <tr>
-        <th></th>
-        <th>Subject</th>
-        <th>Status</th>
-        <th>Sender</th>
-        <th>Date</th>
-      </tr>
-      </thead>
-      <tbody>
-      <router-link
-          :to="{name: 'ehr.transferRequest.show', params: {requestorDID: item.sender.did, fhirTaskID: item.resourceID}}"
-          style="display: contents;"
-          v-for="item in items"
-      >
-        <tr v-bind:class="{ 'hover:bg-gray-100': true, 'cursor-pointer': true, 'none': item.requiresAttention }">
-          <td>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 inline" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" v-if="item.requiresAttention">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 inline" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" v-if="!item.requiresAttention">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76"/>
-            </svg>
-          </td>
-          <td>{{ item.title }}</td>
-          <td>
-            <div class="m-1">
-              <transfer-status :status="item.status"/>
-            </div>
-          </td>
-          <td>{{ item.sender.name }}, {{
-              item.sender.city
-            }}
-          </td>
-          <td>{{ item.date }}</td>
+    <div class="bg-white p-5 shadow-lg rounded-lg">
+      <table class="min-w-full divide-y divide-gray-200">
+        <thead>
+        <tr>
+          <th></th>
+          <th>Subject</th>
+          <th>Status</th>
+          <th>Sender</th>
+          <th>Date</th>
         </tr>
-      </router-link>
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        <router-link
+            :to="{name: 'ehr.transferRequest.show', params: {requestorDID: item.sender.did, fhirTaskID: item.resourceID}}"
+            style="display: contents;"
+            v-for="item in items"
+        >
+          <tr v-bind:class="{ 'hover:bg-gray-50': true, 'cursor-pointer': true, 'none': item.requiresAttention }">
+            <td>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 inline" fill="none" viewBox="0 0 24 24"
+                   stroke="currentColor" v-if="item.requiresAttention">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 inline" fill="none" viewBox="0 0 24 24"
+                   stroke="currentColor" v-if="!item.requiresAttention">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76"/>
+              </svg>
+            </td>
+            <td>{{ item.title }}</td>
+            <td>
+              <transfer-status :status="item.status"/>
+            </td>
+            <td>{{ item.sender.name }}, {{
+                item.sender.city
+              }}
+            </td>
+            <td>{{ item.date }}</td>
+          </tr>
+        </router-link>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script>
--- web/src/ehr/inbox/TransferRequest.vue
+++ web/src/ehr/inbox/TransferRequest.vue
@@ -1,63 +1,88 @@
 <template>
-  <div v-if="transferRequest">
-    <h1 class="page-title">Transfer Request</h1>
+  <div class="px-12 py-8">
+    <button type="button" @click="() => this.$router.push({name: 'ehr.inbox'})" class="btn btn-link mb-10">
+      <span class="w-6 mr-1">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0V0z"
+                                                                                         fill="none"/><path
+            d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z"/></svg>
+      </span>
 
-    <div>
-      <div class="mt-4">
-        <div class="bg-gray-50 font-bold">Requesting Care Organization</div>
+      Back to inbox
+    </button>
+
+    <div v-if="transferRequest">
+      <div class="mb-6 flex items-center justify-between">
+        <h1>Transfer Request</h1>
+
         <div>
-          {{ transferRequest.sender.name }}, {{ transferRequest.sender.city }}
+          <button class="btn btn-primary m-1" @click="complete" v-show="transferRequest.status == 'in-progress'">
+            Complete
+          </button>
+          <button class="btn btn-primary m-1" @click="accept" v-show="transferRequest.status == 'requested'">Accept
+          </button>
+          <button class="btn btn-secondary m-1" @click="reject" v-show="transferRequest.status == 'requested'">Reject
+          </button>
         </div>
       </div>
 
-      <div class="mt-4">
-        <div class="bg-gray-50 font-bold">Status</div>
-        <div>
-          {{ transferRequest.status }}
+      <div class="bg-white rounded-lg shadow-lg">
+        <div class="p-5 bg-gray-50 border-b rounded-t-lg">
+          <h2>Requesting Care Organization</h2>
+
+          <div class="text-gray-700">
+            {{ transferRequest.sender.name }} in {{ transferRequest.sender.city }}
+          </div>
         </div>
-      </div>
 
-      <div class="mt-4">
-        <div class="bg-gray-50 font-bold">Transfer date</div>
-
-        <div>
-          {{ transferRequest.advanceNotice.transferDate }}
-        </div>
-      </div>
-
-      <div class="mt-4">
-        <div class="bg-gray-50 font-bold">Condition</div>
-        <h1>Problems:</h1>
-        <ul class="list-decimal pl-4">
-          <li
-              v-for="patientProblem in transferRequest.advanceNotice.carePlan.patientProblems"
-              class="pl-4"
-          >
-            <p> {{ patientProblem.problem.name }} </p>
+        <div class="p-5">
+          <div>
+            <label>Status</label>
             <div>
-              <h2>Interventions:</h2>
-              <ul class="list-disc">
-                <li v-for="intervention in patientProblem.interventions">
-                  {{ intervention.comment }}
-                </li>
-              </ul>
+              <transfer-status :status="{status: transferRequest.status}"/>
             </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+          </div>
 
-    <div class="mt-4">
-      <button class="btn btn-primary m-1" @click="complete" v-show="transferRequest.status == 'in-progress'">Complete
-      </button>
-      <button class="btn btn-primary m-1" @click="accept" v-show="transferRequest.status == 'requested'">Accept</button>
-      <button class="btn btn-secondary m-1" @click="reject" v-show="transferRequest.status == 'requested'">Reject
-      </button>
+          <div class="mt-4">
+            <label>Transfer date</label>
+
+            <div>
+              {{ transferRequest.advanceNotice.transferDate }}
+            </div>
+          </div>
+
+          <div class="mt-4">
+            <label>Problems</label>
+
+            <ul>
+              <li v-for="patientProblem in transferRequest.advanceNotice.carePlan.patientProblems">
+                <h3 class="font-semibold">Problem</h3>
+
+                <p> {{ patientProblem.problem.name }} </p>
+
+                <div class="mt-2">
+                  <h3 class="font-semibold">Interventions</h3>
+
+                  <ul>
+                    <li v-for="intervention in patientProblem.interventions">
+                      - &nbsp;{{ intervention.comment }}
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import TransferStatus from "../../components/TransferStatus.vue";
+
 export default {
+  components: {
+    TransferStatus
+  },
   data() {
     return {
       transferRequest: null,

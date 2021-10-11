165c5d255439e40e3f2f344abc0995a2c8793d53
Dillen Meijboom <info@excol.nl>
2021-10-11 09:05:00 +0200

    Fixes some minor issues for form errors (#133)

--- web/src/ehr/patient/EditPatient.vue
+++ web/src/ehr/patient/EditPatient.vue
@@ -2,16 +2,18 @@
   <h1 class="mt-10 mb-6">Edit Patient</h1>
 
   <form @submit.stop.prevent="submit">
-    <div class="p-3 bg-red-100 rounded-md" v-if="formErrors.length">
-      <b>Please correct the following error(s):</b>
-      <ul>
-        <li v-for="error in formErrors">* {{ error }}</li>
-      </ul>
-    </div>
+    <div class="bg-white rounded-lg shadow-lg">
+      <div class="sticky top-0 z-10 p-3 bg-red-100 text-red-500 rounded-t-md" v-if="formErrors.length">
+        <label class="text-red-500">Please correct the following error{{ formErrors.length === 0 ? '' : 's' }}:</label>
 
+        <ul class="text-sm">
+          <li v-for="error in formErrors">— {{ error }}</li>
+        </ul>
+      </div>
 
-    <div class="bg-white p-5 rounded-lg shadow-lg">
-      <patient-form :value="patient" mode="edit" @input="(newPatient)=> {patient = newPatient}"/>
+      <div class="p-5">
+        <patient-form :value="patient" mode="edit" @input="(newPatient)=> {patient = newPatient}"/>
+      </div>
     </div>
 
     <div class="mt-4">
@@ -63,9 +65,17 @@ export default {
     },
     checkForm(e) {
       // reset the errors
-      // no checks yet
       this.formErrors.length = 0
-      return true
+
+      if (this.patient.firstName === "" || this.patient.surname === "") {
+        this.formErrors.push("The firstname and surname are required fields.")
+      }
+
+      if (this.patient.dob === null) {
+        this.formErrors.push("The date of birth is a required field.")
+      }
+
+      return this.formErrors.length === 0
     },
     submit() {
       if (!this.checkForm()) {
